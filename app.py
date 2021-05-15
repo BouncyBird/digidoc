42838cf713fc10ff2746418d7833f8c0f1a4ef15
Eshan <eshan.nalajala@gmail.com>
2021-05-15 14:52:47 -0500

    added stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, Markup
 
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField
@@ -13,6 +13,32 @@ import json
 from better_profanity import profanity
 import html2text
 
+disease_links = {
+    'Influenza': 'https://www.cdc.gov/flu/treatment/takingcare.htm',
+    'Ebola': 'https://www.cdc.gov/vhf/ebola/index.html',
+    'Common Cold': 'https://www.cdc.gov/features/rhinoviruses/index.html',
+    'Covid-19': 'https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html',
+    'Malaria': 'https://www.cdc.gov/parasites/malaria/index.html',
+    'Cholerae': 'https://www.cdc.gov/cholera/treatment/index.html'
+}
+
+disease_pages = {
+    'Influenza': 'https://www.cdc.gov/flu/',
+    'Ebola': 'https://www.cdc.gov/vhf/ebola/index.html',
+    'Common Cold': 'https://www.cdc.gov/features/rhinoviruses/index.html',
+    'Covid-19': 'https://www.coronavirus.gov/',
+    'Malaria': 'https://www.cdc.gov/parasites/malaria/index.html',
+    'Cholerae': 'https://www.cdc.gov/cholera/index.html'
+}
+
+disease_extra = {
+    'Influenza': '',
+    'Ebola': 'Ebola can be extremely deadly. <a href="https://www.cdc.gov/vhf/ebola/symptoms/index.html">Symptoms of Ebola</a>. If you think you have Ebola, see a doctor immediately.',
+    'Common Cold': '',
+    'Covid-19': '',
+    'Malaria': '',
+    'Cholerae': ''
+}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1a0303f517b1a1c15d4637c83be89ebc'
@@ -29,10 +55,17 @@ mail = Mail()
 ckeditor.init_app(app)
 mail.init_app(app)
 
+def no_dispose(form, field):
+    if requests.get(f'https://disposable.debounce.io/?email={field.data}').json()['disposable'] == 'true':
+            raise ValidationError('Disposable Emails are not allowed')
+
+def no_profane(form, field):
+    if profanity.contains_profanity(html2text.html2text(field.data)):
+            raise ValidationError('We strictly prohibit profane messages')
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    content = CKEditorField('Your Message', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(), no_dispose])
+    content = CKEditorField('Your Message', validators=[DataRequired(), no_profane])
     recaptcha = RecaptchaField()
     submit = SubmitField('Message')
 
@@ -55,8 +88,7 @@ class DiseaseForm(FlaskForm):
 def get_prediction(data={"Diarreah & Vomit":"Yes","Body Aches":"Yes","Runny Nose":"No","Fever":"Yes","Fatigue":"No","Hemorrhage":"No","Coughing":"Yes","Shortness of Breath":"Yes","Swollen Lymph Nodes":"Yes","Headaches":"Yes","Red eyes":"No","Rapid Heart Rate":"Yes"}):
   url = 'https://5syr7ttrk5.execute-api.us-east-1.amazonaws.com/Predict/88136aae-a71e-4bf4-98e2-50a9c807258a'
   r = requests.post(url, data=json.dumps(data))
-  response = getattr(r,'_content').decode("utf-8")
-  print(response)
+  response = ast.literal_eval(ast.literal_eval(getattr(r,'_content').decode("utf-8"))['body'])
   return response
 
 @app.route('/')
@@ -71,12 +103,6 @@ def about():
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
-        if profanity.contains_profanity(html2text.html2text(form.content.data)):
-            flash('We strictly prohibit profane messages', 'danger')
-            return redirect(url_for('home'))
-        if requests.get(f'https://disposable.debounce.io/?email={form.email.data}').json()['disposable'] == 'true':
-            flash('Disposable Emails are not allowed. Please try again', 'warning')
-            return redirect(url_for('home'))
         tlen = len(form.name.data) + len(form.email.data) + 3
         msg = Message(f'New Message from {form.name.data} - {form.email.data}', sender=form.email.data, recipients=['djonimuresan@gmail.com', 'eshan.nalajala@gmail.com', 'aarnavkumta09@gmail.com', 'matthewcharlotteyang@gmail.com'])
         msg.html = f'<h4>{form.name.data} - {form.email.data}<br>{"=" * tlen}<br></h4>' + form.content.data
@@ -99,14 +125,14 @@ def disease_check():
     if form.validate_on_submit():
         dd = {"Diarreah & Vomit":conv(form.diarreah_vomit.data),"Body Aches":conv(form.body_aches.data),"Runny Nose":conv(form.runny_nose.data),"Fever":conv(form.fever.data),"Fatigue":conv(form.fatigue.data),"Hemorrhage":conv(form.hemorrhage.data),"Coughing":conv(form.coughing.data),"Shortness of Breath":conv(form.shortness_of_breath.data),"Swollen Lymph Nodes":conv(form.swollen_lymph_nodes.data),"Headaches":conv(form.headaches.data),"Red eyes":conv(form.red_eyes.data),"Rapid Heart Rate":conv(form.rapid_heart_rate.data)}
         result = get_prediction(data=dd)
-        res = ast.literal_eval(result)
-        res1 = ast.literal_eval(res['body'])
-        if res1['predicted_label'] == 'Youre all good': 
-            flash(res1['predicted_label'], 'success')
-        elif res1['predicted_label'] == 'Ebola' or res1['predicted_label'] == 'Youre dead':
-            flash(res1['predicted_label'], 'danger')
+        if result['predicted_label'] == 'Youre all good': 
+            flash(result['predicted_label'], 'success')
+        elif result['predicted_label'] == 'Youre dead':
+            flash(result['predicted_label'], 'danger')     
+        elif result['predicted_label'] == 'Ebola':
+            flash(Markup(f'You might have <a href="{disease_pages[result["predicted_label"]]}"><b>{result["predicted_label"]}</b></a>. {disease_extra[result["predicted_label"]]} <a href="{disease_links[result["predicted_label"]]}">Next Steps and More Info</a>'), 'danger')
         else:
-            flash(res1['predicted_label'], 'info')
+            flash(Markup(f'You might have <a href="{disease_pages[result["predicted_label"]]}"><b>{result["predicted_label"]}</b></a>. {disease_extra[result["predicted_label"]]} <a href="{disease_links[result["predicted_label"]]}">Next Steps and More Info</a>'), 'info')
     return render_template("disease.html", title="Disease Checker", form=form, route='disease_check')
 
 @app.route('/clinics')
